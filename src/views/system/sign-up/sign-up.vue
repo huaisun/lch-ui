f59b09dfcc2deba56bcde436ce426bfcba3ba2cf
sunruiguang <420007900@qq.com>
2020-04-22 11:14:29 +0800

    注册布局完善

--- src/views/system/sign-up/sign-up.vue
+++ src/views/system/sign-up/sign-up.vue
@@ -1,8 +1,223 @@
 <template>
-  <div class="login-box"></div>
+  <v-app class="sign-up">
+    <div class="login-box">
+      <h2>注册-LCH</h2>
+      <form class="login-form">
+        <v-text-field dark color="#03e9f4" label="用户名"></v-text-field>
+        <v-text-field dark color="#03e9f4" label="邮箱"></v-text-field>
+        <div class="lch-row">
+          <v-text-field dark color="#03e9f4" label="验证码"></v-text-field>
+          <div class="send-btn">
+            <v-btn
+              dark
+              rounded
+              small
+              :disabled="isSend"
+              @click="sendCode()"
+            >发送验证码{{isSend ? '(' + secondNumber + ')' : ''}}</v-btn>
+          </div>
+        </div>
+        <v-text-field dark color="#03e9f4" label="密码"></v-text-field>
+        <v-row>
+          <v-col cols="6" style="text-align: center;">
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-btn dark icon large color="white" style="margin-top: 30px" v-on="on">
+                  <v-icon dark>undo</v-icon>
+                </v-btn>
+              </template>
+              <span>返回登录</span>
+            </v-tooltip>
+          </v-col>
+          <v-col cols="6">
+            <a href="#">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              提 交
+            </a>
+          </v-col>
+        </v-row>
+      </form>
+    </div>
+  </v-app>
 </template>
 <script>
 export default {
-  name: "sign-up"
+  name: "sign-up",
+  data: () => ({
+    isSend: false,
+    secondNumber: 0
+  }),
+  methods: {
+    sendCode() {
+      this.isSend = true;
+      this.secondNumber = 60;
+      const timeOut = setInterval(() => {
+        if (this.secondNumber <= 0) {
+          clearInterval(timeOut);
+          this.isSend = false;
+        } else {
+          this.secondNumber--;
+        }
+      }, 1000);
+    }
+  }
 };
 </script>
+
+<style scoped>
+.send-btn {
+  margin: 19px 0 19px 20px;
+  width: 120px;
+  text-align: right;
+}
+.login-box {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 400px;
+  padding: 40px;
+  transform: translate(-50%, -50%);
+  background: rgba(0, 0, 0, 0.5);
+  box-sizing: border-box;
+  box-shadow: 0 15px 25px rgba(0, 0, 0, 0.6);
+  border-radius: 10px;
+}
+
+@media screen and (max-width: 400px) {
+  .login-box {
+    width: 100% !important;
+    height: 100% !important;
+  }
+}
+
+.sign-up {
+  font-family: sans-serif;
+  background: linear-gradient(#141e30, #243b55) !important;
+}
+
+.login-box h2 {
+  margin: 0 0 30px;
+  padding: 0;
+  color: #fff;
+  text-align: center;
+}
+
+.login-box .user-box {
+  position: relative;
+}
+
+.login-box form a {
+  position: relative;
+  display: inline-block;
+  padding: 10px 20px;
+  color: #03e9f4;
+  font-size: 16px;
+  text-decoration: none;
+  text-transform: uppercase;
+  overflow: hidden;
+  transition: 0.5s;
+  margin-top: 30px;
+  letter-spacing: 4px;
+  text-align: center;
+  min-width: 125px;
+}
+
+.login-box a:hover {
+  background: #03e9f4;
+  color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 0 5px #03e9f4, 0 0 25px #03e9f4, 0 0 50px #03e9f4,
+    0 0 100px #03e9f4;
+}
+
+.login-box a span {
+  position: absolute;
+  display: block;
+}
+
+.login-box a span:nth-child(1) {
+  top: 0;
+  left: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(90deg, transparent, #03e9f4);
+  animation: btn-anim1 1s linear infinite;
+}
+
+@keyframes btn-anim1 {
+  0% {
+    left: -100%;
+  }
+
+  50%,
+  100% {
+    left: 100%;
+  }
+}
+
+.login-box a span:nth-child(2) {
+  top: -100%;
+  right: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(180deg, transparent, #03e9f4);
+  animation: btn-anim2 1s linear infinite;
+  animation-delay: 0.25s;
+}
+
+@keyframes btn-anim2 {
+  0% {
+    top: -100%;
+  }
+
+  50%,
+  100% {
+    top: 100%;
+  }
+}
+
+.login-box a span:nth-child(3) {
+  bottom: 0;
+  right: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(270deg, transparent, #03e9f4);
+  animation: btn-anim3 1s linear infinite;
+  animation-delay: 0.5s;
+}
+
+@keyframes btn-anim3 {
+  0% {
+    right: -100%;
+  }
+
+  50%,
+  100% {
+    right: 100%;
+  }
+}
+
+.login-box a span:nth-child(4) {
+  bottom: -100%;
+  left: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(360deg, transparent, #03e9f4);
+  animation: btn-anim4 1s linear infinite;
+  animation-delay: 0.75s;
+}
+
+@keyframes btn-anim4 {
+  0% {
+    bottom: -100%;
+  }
+
+  50%,
+  100% {
+    bottom: 100%;
+  }
+}
+</style>
